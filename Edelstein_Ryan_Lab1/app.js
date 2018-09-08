52f3f01a7f9e5edf310947153b36912071cb8a28
Ryan Edelstein <redelste@stevens.edu>
2018-09-07 21:59:38 -0400

    added a logger middleware

--- Edelstein_Ryan_Lab1/app.js
+++ Edelstein_Ryan_Lab1/app.js
@@ -3,20 +3,38 @@ const express = require("express");
 const app = express();
 const configRoutes = require("./routes");
 const bodyParser = require("body-parser");
+let urlCounts = {}
 
-app.use(bodyParser.json());
 
-configRoutes(app);
-
-var myLogger = function(req,res,next){
-  console.log('Logged')
+const myLogger = function(req,res,next){
+  console.log("\x1b[33m%s\x1b[0m", "-----------------", '\x1b[0m')
+  console.log('\x1b[36m%s\x1b[0m', "LOGGER", '\x1b[0m');
+  console.log("\x1b[33m%s\x1b[0m", "-----------------", '\x1b[0m')
+  console.log('REQUEST BODY: ', req.body)
+  console.log('ENDPOINT:', req.originalUrl);
+  console.log('HTTP VERB:', req.method);
   next()
 }
-// app.use(myLogger)
-//
-// app.get('/',function(req,res){
-//   res.send("hello World!")
-// })
+
+const myLoggerCounter = function(req,res,next){
+  if (!(req.originalUrl in urlCounts)){
+    urlCounts[req.originalUrl] = 1
+  } else{
+    urlCounts[req.originalUrl] += 1
+  }
+  console.log("\x1b[33m%s\x1b[0m", "-----------------", '\x1b[0m')
+  console.log('\x1b[36m%s\x1b[0m', "URL COUNTER", '\x1b[0m');
+  console.log("\x1b[33m%s\x1b[0m", "-----------------", '\x1b[0m')
+  console.log(req.originalUrl, " was hit ",urlCounts[req.originalUrl], (urlCounts[req.originalUrl] == 1) ? "time." : " times.");
+  next()
+}
+
+//middlewares
+app.use(bodyParser.json());
+app.use(myLogger)
+app.use(myLoggerCounter)
+configRoutes(app);
+
 
 app.listen(3000, () => {
   console.log("We've now got a server!");
