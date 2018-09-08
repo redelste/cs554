6d678d51a80c086379c5bb5db7e5270d8993e91a
Ryan Edelstein <redelste@stevens.edu>
2018-09-07 22:02:21 -0400

    removed extraneous files

--- Edelstein_Ryan_Lab1/routes/index.js
+++ Edelstein_Ryan_Lab1/routes/index.js
@@ -1,10 +1,6 @@
-const postRoutes = require("./posts");
-const userRoutes = require("./users");
 const taskRoutes = require("./tasks");
 
 const constructorMethod = app => {
-  app.use("/posts", postRoutes);
-  app.use("/users", userRoutes);
   app.use("/api/tasks", taskRoutes);
   app.use("*", (req, res) => {
     res.status(404).json({ error: "Not found" });
